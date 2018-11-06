8d4cb5ca7e3051fd1db9ede614322d1d072c7609
Daniel Plawgo <daniel@plawgo.pl>
2018-11-06 05:23:46 +0100

    dodanie przykładu

---
+++ PartialForms/PartialForms/Controllers/UsersController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PartialForms.ViewModels.Users;
+
+namespace PartialForms.Controllers
+{
+    public class UsersController : Controller
+    {
+        public ActionResult Partial()
+        {
+            return View(new UserViewModel());
+        }
+
+        [HttpPost]
+        public ActionResult Partial(UserViewModel viewModel)
+        {
+            return View(viewModel);
+        }
+
+        public ActionResult EditorTemplate()
+        {
+            return View(new UserViewModel());
+        }
+
+        [HttpPost]
+        public ActionResult EditorTemplate(UserViewModel viewModel)
+        {
+            return View(viewModel);
+        }
+    }
+}
---
+++ PartialForms/PartialForms/ViewModels/Users/UserViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PartialForms.ViewModels.Shared;
+
+namespace PartialForms.ViewModels.Users
+{
+    public class UserViewModel
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public AddressViewModel Address { get; set; } = new AddressViewModel();
+    }
+}
